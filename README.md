4ce8d28e37a5a67c99cff396754681e29f1077f8
Po-chia Chen <chen.buo.jia@gmail.com>
2021-09-14 16:03:55 +1000

    Capability to read list of frames to be discarded.
    Added new functionality for more complex repair procedures.
    
    The idea is the run scan, and then isolate all frame-IDs based on, e.g.,
    an irregular frame siz. The list of frames (1-st column in scan file)
    can then be given for subtraction.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gmx_rescue
+
+Compile Instructions:
+
+Is fairly trivial, using a recent GCC compiler.
+
+gcc gmx_rescue.c -o gmx_rescue
--- gmx_rescue.c
+++ gmx_rescue.c
@@ -78,6 +78,8 @@
 #define BUF_SIZE 1024*32
 #define VAR_SIZE 4
 
+#define FRAME_BUF_SIZE 1024*1024*32
+
 // Source: https://www.geeksforgeeks.org/write-an-efficient-c-program-to-reverse-bits-of-a-number
 unsigned int reverse_bits(unsigned int num)
 {
@@ -97,7 +99,7 @@ unsigned int reverse_bits(unsigned int num)
 }
 
 // source: https://codereview.stackexchange.com/questions/151049/endianness-conversion-in-cnsigned int reverse_endain
-inline uint32_t reverse_endian(uint32_t value) 
+uint32_t reverse_endian(uint32_t value) 
 {
     return (((value & 0x000000FF) << 24) |
             ((value & 0x0000FF00) <<  8) |
@@ -112,6 +114,14 @@ struct frameHeader {
     int step ;
     float timeStamp;
     // float box[3][3] ;
+} ;
+
+struct frameBuffer {
+    int magic;
+    unsigned int nAtoms;
+    int size   ;
+    int nAlloc ;
+    int *buffer ;
 } ;
 
 void print_header(struct frameHeader *h) {
@@ -134,6 +144,7 @@ struct frameHeader read_header(FILE *fp, bool bReverse) {
     h.timeStamp = *(float*)&temp ;
     return h ;
 }
+
 
 // source: https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
 // little endian
@@ -168,40 +179,109 @@ void debug_stream(FILE * fp, int items, bool bReverse) {
     }
 }
 
+struct frameBuffer init_frameBuffer(int magic) {
+    struct frameBuffer frame;
+    frame.magic  = magic;
+    frame.nAtoms = 0;
+    frame.nAlloc = 0;
+    frame.size   = 0;
+    frame.buffer = NULL;
+    return frame;
+}
+
+void clean_frameBuffer(struct frameBuffer *f){
+    if ( f->buffer != NULL ) { free(f->buffer) ; }
+}
+
+void read_frame_into_buffer(FILE *fp, struct frameBuffer* pFrame, bool bReverse, bool bFirst) {
+    int a ;
+    struct frameHeader h;
+    //  Start with safety checks
+    fread( &a, VAR_SIZE, 1 , fp );
+    if ( a != pFrame->magic ) {
+        fprintf(stderr,"= = ERROR: file pointer is not at a MAGIC number! : %ld\n", ftell(fp)-VAR_SIZE);
+        fseek(fp, -1*VAR_SIZE, SEEK_CUR);
+        return;
+    }
+    h = read_header( fp, bReverse ) ;
+    if ( bFirst ) { pFrame->nAtoms = h.nAtoms ; }
+    if ( h.nAtoms != pFrame->nAtoms ) {
+        fprintf(stderr,"= = ERROR: file pointer is at a MAGIC number, but the next int is not numAtoms! : %ld\n", ftell(fp)-VAR_SIZE );
+        fseek(fp, -4*VAR_SIZE, SEEK_CUR);
+        return;
+    }
+
+    // Now start storing into memory
+    if ( bFirst == true ) {
+        pFrame->nAlloc = FRAME_BUF_SIZE;
+        pFrame->buffer = (int*)malloc(pFrame->nAlloc);
+    }
+    pFrame->buffer[0] = a;
+    fseek(fp, -3*VAR_SIZE, SEEK_CUR);
+    pFrame->size = 1;
+
+    while ( !( feof(fp) || ferror(fp) ) ) {
+        fread( &a, VAR_SIZE, 1 , fp ) ;
+        // First check for magic numbers.
+        if ( a == pFrame->magic ) {
+            // Then check for the rest of the header.
+            h = read_header( fp, bReverse ) ;
+            if ( h.nAtoms == pFrame->nAtoms ) {
+                // Reset to magic number position after reading headfer.
+                fseek(fp, -4*VAR_SIZE, SEEK_CUR);
+                break;
+            } 
+        }
+        // Store value, and resize buffer as necessary.
+        pFrame->buffer[pFrame->size] = a ; ++pFrame->size;
+        if ( pFrame->size == pFrame->nAlloc ) {
+            pFrame->nAlloc += FRAME_BUF_SIZE ;
+            pFrame->buffer = (int*)realloc(pFrame->buffer, pFrame->nAlloc);
+        }
+    }
+    // File end or frame end.
+    return;
+}
+
+void debug_frameBuffer(struct frameBuffer *f) {
+    printf("= = Frame buffer contents:");
+    printf("Magic:  %d\n", f->magic);
+    printf("nAtoms: %d\n", f->nAtoms);
+    printf("Size:   %d\n", f->size);
+    printf("Buffer: %p\n", f->buffer);
+}
 
 void print_usage(char *argv[]) {
-    fprintf(stderr, "usage: %s scan   <inputFile> [outputFile] : finds frames by magic numbers, optional output to text file.\n", argv[0]);
-    fprintf(stderr, "       %s rescue <inputFile> <outputFile> <Offset>: print file starting from offset\n", argv[0]);
-    fprintf(stderr, "       %s auto   <inputFile> <outputFile> : attempt automatic repair assuming first frame is OK\n", argv[0]);
-    fprintf(stderr, "       %s query  <inputFile> <Offset> [context]: print raw-byte data around offset\n", argv[0]);
+    fprintf(stderr, "usage: %s scan   <inputFile> [outputFile] : finds frames by (magic_number,nAtom) pair, optional output to text file.\n", argv[0]);
+    fprintf(stderr, "       %s rescue <inputFile> <outputFile> <Offset>: find valid magic number starting from 4-byte offsets then print rest of file.\n", argv[0]);
+    fprintf(stderr, "       %s repair <inputFile> <outputFile> <discardFile>: discard frames, given as a text file containing an increasing list of integers.\n", argv[0]);
+//    fprintf(stderr, "       %s auto   <inputFile> <outputFile> : attempt automatic repair assuming first frame is OK\n", argv[0]);
+    fprintf(stderr, "       %s query  <inputFile> <Offset> [context]: print raw 4-byte binary data around offset, plus integer/float conversions.\n", argv[0]);
+    fprintf(stderr, "       ...Note: As endian-ness depends on architecture, the magic number %d of the fixed big-endian XTC might be reported as %d\n", MAGIC_BIG_ENDIAN, MAGIC_LITTLE_ENDIAN );
 }
 
 int main(int argc,  char *argv[]) {
 
     off64_t  f, out;
     FILE *fp, *fpOut;
-    int value, magic, i, nAtoms = -1;
+    int value, magic, i=0, nAtoms = -1;
     unsigned int temp;
     struct frameHeader header;
     bool bReverse;
-    off64_t res, res2, frames, offset, offsetPrev;
-    char buf[BUF_SIZE];
+    off64_t res=0, res2=0, frames=0, offset=0, offsetPrev=0;
 
-    if (argc<3) {
-        print_usage(argv);
-        return 10;
-    }
+    if (argc<3) { print_usage(argv); return 10; }
 
     // open xtc file readonly
     fp = fopen(argv[2], "rb");
     if ( fp == NULL ) {
-        perror("= = Couldn't open file for reading");
+        fprintf(stderr,"= = Couldn't open file for reading\n");
         return 20;
     }
     // Check endianness of CPU versus the big-endian of the XTC.
     res = fread( &value, VAR_SIZE, 1 , fp ) ;
     if ( feof(fp) || ferror(fp) ) {
-        perror("= = File is either empty or other error has occurred");
+        fprintf(stderr,"= = File is either empty or other error has occurred\n");
         return 20;
     }
     switch( value ) {
@@ -214,7 +294,7 @@ int main(int argc,  char *argv[]) {
             bReverse = false ;
             break;
         default:
-            perror("= = First value read does not correspond to the magic number for file integrity check!");
+            fprintf(stderr,"= = First value read does not correspond to the magic number for file integrity check!\n");
             return 20;
     }
     // Reset.
@@ -231,29 +311,24 @@ int main(int argc,  char *argv[]) {
         fprintf(fpOut, "%-12s %-12s %-12s %-12s %-12s %-s\n", "Frame", "numAtoms", "Step", "Time", "Delta", "Offset");
         offsetPrev=0 ; frames=0; res=1;
         while ( !( feof(fp) || ferror(fp) ) ) {
-            // This steps seems to randomly catch a comparison that clearly shouldn't be equal.
+
             res = fread( &value, VAR_SIZE, 1 , fp ) ;
+            // First check for magic numbers.
             if ( value == magic ) {
-                //debug_stream(fp, 3);
+                // Then check for the rest of the header.
                 header = read_header( fp, bReverse ) ;
                 offset = ftell(fp)/VAR_SIZE - 4 ;
-                if ( nAtoms==-1 ) {
-                    nAtoms = header.nAtoms ; 
-                }
+                if ( nAtoms==-1 ) { nAtoms = header.nAtoms ; }
                 if ( nAtoms == header.nAtoms ) {
-                    fprintf(fpOut, "%12d %12d %12d %12e %12ld %ld\n",
+                    fprintf(fpOut, "%12ld %12d %12d %12e %12ld %ld\n",
                             frames, header.nAtoms, header.step, header.timeStamp, offset-offsetPrev , offset );
                     frames++ ;
                     offsetPrev = offset ;
                 } 
             }
         }
-        if ( ferror(fp) ) {
-            fprintf(stderr, "...File read error encountered!\n");          
-        }
-        if ( feof(fp) ) {
-            fprintf(stderr, "...End of file reached.\n");
-        }
+        if ( ferror(fp) ) { fprintf(stderr,"...File read error encountered!\n"); }
+        if ( feof(fp)   ) { fprintf(stderr,"...End of file reached.\n"); }
         fclose(fp);
         fclose(fpOut);
         return 0;
@@ -261,6 +336,8 @@ int main(int argc,  char *argv[]) {
 
     // Old rescue mechanic that prints everything after the offset.
     if ( strcmp(argv[1], "rescue") == 0 ) {
+        if ( argc<5 ) { fprintf(stderr,"= = ERROR: Not enough arguments given.\n"); return 10; }
+        char buf[BUF_SIZE];       
         char *ptr;
         off64_t offsetNew;
         offset = strtol(argv[4], &ptr, 10);
@@ -271,12 +348,12 @@ int main(int argc,  char *argv[]) {
             ;
         }
         if ( ferror(fp) ) {
-            fprintf(stderr, "ERROR: ...File read error encountered!\n");
+            fprintf(stderr, "= = ERROR: ...File read error encountered!\n");
             fprintf(stderr, "     ftell position: %ld\n", ftell(fp) );
             return 40;
         }
         if ( feof(fp) ) {
-            fprintf(stderr, "ERROR: ...End of file reached first!\n");
+            fprintf(stderr, "= = ERROR: ...End of file reached first!\n");
             fprintf(stderr, "     ftell position: %ld\n", ftell(fp) );
             return 40;
         }
@@ -287,7 +364,7 @@ int main(int argc,  char *argv[]) {
         fseek(fp, offsetNew*VAR_SIZE, SEEK_SET);
         // Test if output file exists.
         if( access( argv[3], F_OK ) == 0 ) {
-            printf("= = ERROR: Please delete the existing output file %s.\n", argv[3]);
+            fprintf(stderr,"= = ERROR: Please delete the existing output file %s.\n", argv[3]);
             return 30;                                                               
         }
     
@@ -302,29 +379,69 @@ int main(int argc,  char *argv[]) {
         return 0;
     }
 
-    // Attempt automated repair?
-    if ( strcmp(argv[1], "auto") == 0 ) {
-        if( access( argv[3], F_OK ) == 0 ) {
-            printf("= = ERROR: Please delete the existing output file %s.\n", argv[3]);
+    // More complex Automated repair routine that snips out selected frames.
+    if ( strcmp(argv[1], "repair") == 0 ) {
+        if ( argc<5 ) { fprintf(stderr,"= = ERROR: Not enough arguments given.\n"); return 10; }
+        if ( access( argv[3], F_OK ) == 0 ) {
+            fprintf(stderr,"= = ERROR: Please delete the existing output file %s.\n", argv[3]);
             return 30;                                                               
         }
-        // The plan is to parse the input file by frames and identify frames that would be sanely added.
-        fpOut = fopen( argv[3], "wb" );
-
-        frames=0; res=1;
-        while ( !( feof(fp) || ferror(fp) ) ) {
-            res = fread( &value, VAR_SIZE, 1 , fp ) ;
-            if ( value == magic ) {
-                //debug_stream(fp, 3);
-                header = read_header( fp, bReverse ) ;
-                //print_header( &header ) ; 
-                frames++;
-            }
+        if ( access( argv[4], F_OK ) != 0 ) {
+            fprintf(stderr,"= = ERROR: The frame file %s is missing or cannot be opened.\n", argv[4]);
+            return 30;                                                               
         }
 
-        
+        // Read frame file and encode list.
+        FILE   *fpFrames;
+        char   *line = NULL;
+        size_t nChar = 0;
+        int    *arrDelFrames = NULL;
+        int    nArrDelFrames = 0, nFrameMalloc = 10, stepsFrameMalloc = 10;
+        int    iDelFrame = 0 ;
+        struct frameBuffer frame;
+
+        fpFrames = fopen(argv[4], "r");
+        arrDelFrames = (int*) malloc(nFrameMalloc*sizeof(int));
+        while ( getline(&line, &nChar, fpFrames) >= 0 ) {
+            sscanf(line, "%d\n", &arrDelFrames[nArrDelFrames]);
+            nArrDelFrames++;
+            if ( nArrDelFrames==nFrameMalloc ) {
+                nFrameMalloc += stepsFrameMalloc ;
+                arrDelFrames = (int*) realloc(arrDelFrames, nFrameMalloc*sizeof(int));
+            }
+        }
+        //printf("Debug\n");
+        //for(i=0;i<nArrDelFrames;++i) {
+        //    printf("%d ", arrDelFrames[i]);
+        //}
+        //printf("\n");
+
+        fpOut = fopen( argv[3], "wb" );
+
+        // The plan is to parse the input file per frame and write it out frame by frame.
+        // Do the first frame separately. Does not take into account single-frame trajaectories.
+        frame = init_frameBuffer( magic ); i=0;
+        read_frame_into_buffer(fp, &frame, bReverse, true) ;
+        debug_frameBuffer(&frame);
+        while ( !( feof(fp) || ferror(fp) ) ) {
+            if ( arrDelFrames[iDelFrame] == i ) {
+                printf("...skipping frame %d\n", i);
+                ++iDelFrame;
+            } else {
+                printf("...writing frame %d\n", i);
+                fwrite( frame.buffer, 1, frame.size*VAR_SIZE, fpOut) ;
+            }
+            read_frame_into_buffer(fp, &frame, bReverse, false);
+            //debug_frameBuffer(&frame);
+            ++i;
+        }
+        if ( ferror(fp) ) { fprintf(stderr,"...File read error encountered!\n"); }
+        if ( feof(fp)   ) { fprintf(stderr,"...End of file reached.\n"); }
         fclose(fp);
         fclose(fpOut);
+        
+        clean_frameBuffer( &frame );
+        free( arrDelFrames );
         return 0;
     }
 
@@ -333,15 +450,14 @@ int main(int argc,  char *argv[]) {
         char *ptr;
         int range = 10;
         off64_t offPre ;
-        if ( argc == 5 ) {
-            range=atoi(argv[4]);
-        }
+        if ( argc<4 ) { fprintf(stderr,"= = ERROR: Not enough arguments given.\n"); return 10; }
+        if ( argc == 5 ) {range=atoi(argv[4]);}
         offset = strtol(argv[3], &ptr, 10);
         // reposition file offset by setting it to offset bytes
         offPre = (offset > range) ? offset - range : 0 ;
         fseek(fp, offPre * VAR_SIZE, SEEK_SET);
         debug_stream(fp, (offset-offPre), bReverse);
-        puts("-------- -------- -------- --------");
+        puts("-------- -------- -------- -------- :   <integer>  :     <float>");
         debug_stream(fp, 1, bReverse);
         puts("-------- -------- -------- --------");
         debug_stream(fp, range, bReverse);
@@ -349,5 +465,10 @@ int main(int argc,  char *argv[]) {
         fclose(fp);
         return 0;
     }
+
+    // Default path when the arguments don't fit anything.
+    fprintf(stderr,"= = ERROR: the first argument is not a recognised command.\n");
+    print_usage(argv);
+    return 99;
 
 }
